237d050c17bb22defd193fbf272bd83642af1bbd
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2018-06-19 16:43:52 -0400

    big refactor, better commments for styleguide builder script, gonna move it and delete this file tho

--- pug-build-test.js
+++ pug-build-test.js
@@ -4,54 +4,60 @@ var pug = require('pug');
 // Import glob for file matching.
 var glob = require('glob');
 
-// TODO: wanna include all partials/components, render them with a template
-// then compile to an 'all-components.html' 'master' component to include.
 
-// Get all components by reading the file system in styleguide/components.
+// Get the components dir from package config.
+var componentsDir = process.env.npm_package_config_componentsDir;
 
-// Make a function to call pug compile.
-// HACK: gotta include filename opt for include in template. Weird.
-
-var components = [];
-
-glob('./styleguide/components/*.html', {
-  nodir: true,
-},
-function(err, files) {
-  if (files) {
-    components = files;
-    buildComponents(components);
-  }
-});
+// Check if we have components dir, if not, exit(?).
+if (componentsDir) {
+  findComponentFiles(componentsDir);
+} else {
+  // Throw some kinda error.
+  process.exit();
+}
 
 
+// Finds files for components.
+// Get all components by reading the file system in componentsDir.
+function findComponentFiles(componentsDir) {
+  // Do a search in the componentsDir.
+  glob(`./styleguide/${componentsDir}/*.html`, {
+    nodir: true,
+  },
+  function(err, files) {
+    // If found...
+    if (files) {
+      var components = files;
+      // Build em.
+      buildComponents(components);
+    }
+  });
+};
+
+// Builds files for components into HTML.
 function buildComponents(components) {  
   var renderResults = '';
   components.forEach(component => {
-    
-    // A component template.
+    // Get the name of the component.
     var fileName = component.split('components/')[1];
     
+    // A component template. //// NB!: The indentation of this template literal is important! 
     var compileSrc = `
 .component-example
   h1='${fileName.split('.html')[0]}'
   hr
   .component-wrapper
-    include styleguide/components/${fileName}
-  `;
+    include styleguide/${componentsDir}/${fileName}
+    `;
+    // HACK: gotta include filename opt for include in template. Weird.
+    // Make a function to call pug compile.
     var render = pug.compile(compileSrc, {'filename':'foo'});
     // So, call render for each component, concat to a string and output that to 'all-components.htm' or a local variable for index.pug to consume.  Good thing w/ the former is that its very explicit.
     
     renderResults+=render();
   });
-  // console.log(renderResults)
-  // process.exit();
   
-  
-  
-  // Test it.
-  // console.log(renderResults);
-  // Test it w/ rendering index.pug w/ options.
+  // Assign locals for rendering index.pug w/ options.
   var locals = {
     allComponents: renderResults,
     commonVariables: {
@@ -67,6 +73,7 @@ function buildComponents(components) {
   var html = pug.renderFile('./styleguide/index.pug', locals);
   
   // Output the HTML rendered from style guide template and variables.
+  // TODO this could actually write to a file instead of STDOUT.
   console.log(html);
 
 }
